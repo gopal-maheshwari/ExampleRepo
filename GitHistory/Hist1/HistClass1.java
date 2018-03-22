d93eaad64bd357e789c5252fd16a4111abf92ea2
Gopal <gopal.maheshwari@ascendlearning.com>
2018-03-22 08:43:11 +0530

    commit_history

--- GitHistory/Hist1/HistClass1.java
+++ GitHistory/Hist1/HistClass1.java
@@ -4,7 +4,7 @@ public class HistClass1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("History Git Message");
+		System.out.println("History Git Message New");
 	}
 
 }
