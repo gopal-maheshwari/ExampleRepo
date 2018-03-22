00da1fafddae5c91acf4d04d502d29ad96388d39
Gopal <gopal.maheshwari@ascendlearning.com>
2018-03-22 08:40:31 +0530

    first commit no changes after sharing
    Signed-off-by: Gopal <gopal.maheshwari@ascendlearning.com>

---
+++ GitHistory/Hist1/HistClass1.java
@@ -0,0 +1,10 @@
+package Hist1;
+
+public class HistClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("History Git Message");
+	}
+
+}
